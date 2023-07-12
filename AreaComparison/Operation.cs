024e20449a1534f5ca0c05329a9cb75da3b2d8a6
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-12 14:52:42 +0530

    [Riya] Add : Implemented Area Calculation

---
+++ AreaComparison/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaComparison
+{
+    public class Operation
+    {
+        int l, b;
+        public Operation(int l, int b) 
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public void CalculateArea()
+        {
+            double area = l * b;
+            Console.WriteLine("Area of rectangle : " + area);
+        }
+    }
+}
--- AreaComparison/Program.cs
+++ AreaComparison/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace AreaComparison
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Area Comparison problem statement");
+            Operation operation = new Operation(10, 5);
+            operation.CalculateArea();
+            //Operation operation2 = new Operation(100, 50);
+            //double line2 = operation2.CalculateArea();
+
+        }
+    }
+}
